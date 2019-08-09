2ee70144bdfa86043b6f66384a6f01d1d3f60c9a
Tianlin Zheng <zhengt@mit.edu>
2019-08-09 15:07:01 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,8 @@
 def square_array(array)
-  # your code here
+  counter = 0 
+  while counter < array.length do 
+    array[counter] = array[counter] ** 2 
+    counter += 1 
+  end
+  return array
 end
